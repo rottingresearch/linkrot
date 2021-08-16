53c051e1398251dc086540b91f7ccfb6e2cc2d2f
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2021-08-16 10:25:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.81",
+    version="0.986",
 
     description='Extract metadata and URLs from PDF files'
         ', and check URL status.',
