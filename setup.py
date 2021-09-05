5836574ff912f8cfefd6d57e378bc08e5aceabdc
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2021-09-05 19:30:03 -0400

    Trying something old

--- setup.py
+++ setup.py
@@ -11,11 +11,13 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+here = path.abspath(path.dirname(__file__))
+reqs = parse_requirements(path.join(here, "requirements.txt"))
 
 setup(
     name='linkrot',
@@ -23,18 +25,21 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.3.1",
+    version="1.1",
 
     description='Extract metadata and URLs from PDF files'
-        ', and download all referenced PDFs',
-    long_description=long_description,
-    long_description_content_type='text/markdown'
+        ', and check URL status.',
+    long_description='This is the long description.',
+
+    # The project's main homepage.
+    url='https://github.com/marshalmiller/linkrot',
+
     # Author details
-    author='Marshal A. Miller',
+    author='Marshal Miller',
     author_email='marshal@marshalmiller.com',
 
     # Choose your license
-    license='Apache',
+    license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -46,30 +51,17 @@ setup(
 
         'Environment :: Console',
 
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+
 
         'Topic :: Software Development :: Build Tools',
         'Topic :: Scientific/Engineering',
@@ -78,11 +70,11 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='pdf extract download urls',
+    keywords='pdf reference linkrot',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=["tests"]),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -92,19 +84,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        'pdfminer2',
-        'chardet'
-    ],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['tox'],
-    },
+    install_requires=reqs,
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
@@ -123,8 +103,8 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': [
-            'linkrot=linkrot.cli:main',
+        "console_scripts": [
+            "linkrot=linkrot.cli:main",
         ],
     },
 )
