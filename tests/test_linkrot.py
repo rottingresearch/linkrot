b0f2a6d70c5adc82bf96af6e9ab968b4dd1bcf97
Marshal Miller <marshal@marshalmiller.com>
2021-10-03 10:20:45 -0400

    Uploading PyPi Fixes

---
+++ tests/test_linkrot.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import linkrot
+import pytest
+
+curdir = os.path.dirname(os.path.realpath(__file__))
+
+
+def test_all():
+    with pytest.raises(linkrot.exceptions.FileNotFoundError):
+        linkrot.linkrot("asd")
+
+    with pytest.raises(linkrot.exceptions.DownloadError):
+        linkrot.linkrot("http://invalid.com/404.pdf")
+
+    with pytest.raises(linkrot.exceptions.PDFInvalidError):
+        linkrot.linkrot(os.path.join(curdir, "pdfs/invalid.pdf"))
+
+    pdf = linkrot.linkrot(os.path.join(curdir, "pdfs/valid.pdf"))
+    urls = pdf.get_references(reftype="pdf")
+    assert len(urls) == 18
+    # pdf.download_pdfs("/tmp/")
+
+
+def test_two_pdfs():
+    # See https://github.com/metachris/linkrot/issues/14
+    linkrot.linkrot(os.path.join(curdir, "pdfs/i14doc1.pdf"))
+    pdf_2 = linkrot.linkrot(os.path.join(curdir, "pdfs/i14doc2.pdf"))
+    assert len(pdf_2.get_references()) == 2
