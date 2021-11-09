65f40654382ef3f503b56c37cba3ee4127b88143
Vansh Comar <vanshcomar1@gmail.com>
2021-11-09 17:00:12 +0530

    Formatting fix

--- linkrot/colorprint.py
+++ linkrot/colorprint.py
@@ -4,11 +4,11 @@ from __future__ import absolute_import, division, print_function,\
 
 # Following are the ANSI octal escape sequences for different formatting
 
-HEADER = "\033[95m" 
-OKBLUE = "\033[94m" 
-OKGREEN = "\033[92m" 
-WARNING = "\033[93m" 
-FAIL = "\033[91m" 
+HEADER = "\033[95m"
+OKBLUE = "\033[94m"
+OKGREEN = "\033[92m"
+WARNING = "\033[93m"
+FAIL = "\033[91m"
 
 # Text Style
 BOLD = "\033[1m"
