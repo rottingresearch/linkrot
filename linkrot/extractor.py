ba5965fa708ded37f0b9056f1412d8ba48e7ca58
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2021-11-21 19:59:13 -0500

    Attempting to Fix URL Regex

--- linkrot/extractor.py
+++ linkrot/extractor.py
@@ -19,34 +19,25 @@ ARXIV_REGEX2 = r"""arxiv.org/abs/([^\s,]+)"""
 # DOI
 DOI_REGEX = r"""DOI:\s?([^\s,]+)"""
 
-# URL
-URL_REGEX = r"""(?xi)
-\b
-(							# Capture 1: entire matched URL
-  (?:
-    [a-z][\w-]+:				# URL protocol and colon
-    (?:
-      /{1,3}						# 1-3 slashes
-      |								#   or
-      [a-z0-9%]						# Single letter or digit or '%'
-      								# (Trying not to match e.g. "URI::Escape")
+
+# 2 line regex for URLs
+URL_REGEX = r"""
+    (?i)\b                                            # Start at word boundary
+    (?:                                                # Capture possible
+        [a-z][\w-]+:                                   # URL protocol and colon
+        (?:                                            # Rest of URL
+            /{1,3}                                     # 1-3 slashes
+            [a-z0-9%]                                  # Single letter/digit/percent
+            [a-z0-9.\-_~%!$&'()*+,;=:@/]*              # Rest of URL
+        )                                              # End rest of URL
+    |                                                  # Or
+        (?:                                            # Capture possible
+            [a-z0-9.\-]+                               # Domain
+            \.                                         # Period
+        )*                                             # Rest of domain
+        [a-z]{2,}                                      # TLD
+        \b                                             # End at word boundary
     )
-    |							#   or
-    www\d{0,3}[.]				# "www.", "www1.", "www2." … "www999."
-    |							#   or
-    [a-z0-9.\-]+[.][a-z]{2,4}/	# looks like domain name followed by a slash
-  )
-  (?:							# One or more:
-    [^\s()<>]+						# Run of non-space, non-()<>
-    |								#   or
-    \(([^\s()<>]+|(\([^\s()<>]+\)))*\)	# balanced parens, up to 2 levels
-  )+
-  (?:							# End with:
-    \(([^\s()<>]+|(\([^\s()<>]+\)))*\)	# balanced parens, up to 2 levels
-    |									#   or
-    [^\s`!()\[\]{};:'".,<>?«»“”‘’]		# not a space or one of these punct char
-  )
-)
 """
 
 def extract_urls(text):
