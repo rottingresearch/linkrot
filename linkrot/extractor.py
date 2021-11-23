2e7f433019b43abdd335326f71c5c9145665c3f7
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2021-11-22 19:16:00 -0500

    Attempt to Fix URL Regex
    Changed to single line formate for now. Make sure to compress to one later.

--- linkrot/extractor.py
+++ linkrot/extractor.py
@@ -20,25 +20,8 @@ ARXIV_REGEX2 = r"""arxiv.org/abs/([^\s,]+)"""
 DOI_REGEX = r"""DOI:\s?([^\s,]+)"""
 
 
-# 2 line regex for URLs
-URL_REGEX = r"""
-    (?i)\b                                            # Start at word boundary
-    (?:                                                # Capture possible
-        [a-z][\w-]+:                                   # URL protocol and colon
-        (?:                                            # Rest of URL
-            /{1,3}                                     # 1-3 slashes
-            [a-z0-9%]                                  # Single letter/digit/percent
-            [a-z0-9.\-_~%!$&'()*+,;=:@/]*              # Rest of URL
-        )                                              # End rest of URL
-    |                                                  # Or
-        (?:                                            # Capture possible
-            [a-z0-9.\-]+                               # Domain
-            \.                                         # Period
-        )*                                             # Rest of domain
-        [a-z]{2,}                                      # TLD
-        \b                                             # End at word boundary
-    )
-"""
+# 2 line regexes
+URL_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re"""
 
 def extract_urls(text):
     """
