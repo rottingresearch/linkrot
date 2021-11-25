68086e7ec72490c52b4500fb851396f17a562da4
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2021-11-24 22:18:30 -0500

    Retrying Original REGEX Method

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="2.9",
+    version="2.91",
 
     # Package Descriptions
     description='Extract metadata and URLs from PDF files',
