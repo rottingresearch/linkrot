dc5bf0f1477cc4f0f5bd4c0be405fdb338dd3e22
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2022-05-31 20:59:38 -0400

    Update setup.py
    Added fix to exclude email addresses

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="3.4",
+    version="3.5",
 
     # Package Descriptions
     description='Extract metadata and URLs from PDF files',
