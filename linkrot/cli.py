96130ec4c3d9e4dbafd34b7a7f5dd45747f90177
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2022-07-29 19:55:18 -0400

    Remove Flake8 comment

--- linkrot/cli.py
+++ linkrot/cli.py
@@ -21,7 +21,7 @@ from linkrot.downloader import check_refs
 IS_PY2 = sys.version_info < (3, 0)
 if IS_PY2:
     # Python 2
-    parse_str = unicode_ # noqa: F821
+    parse_str = unicode_
 else:
     # Python 3
     parse_str = str
