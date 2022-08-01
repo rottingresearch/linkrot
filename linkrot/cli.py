357b5546d9111ec947fd438c3c530142b180fc21
Marshal Miller <marshal@marshalmiller.com>
2022-08-01 12:09:28 -0400

    Commented Out Metadata Parser
    Trying to fix Issue #123

--- linkrot/cli.py
+++ linkrot/cli.py
@@ -124,8 +124,10 @@ def get_text_output(pdf, args):
     """ Normal output of infos of linkrot instance """
     # Metadata
     ret = ""
-    # ret += "Document infos:\n"
-    # print(pdf.get_metadata()[None])
+    ret += "Document infos:\n"
+
+    # Remove Sorting to deal with NoneType Error for now. Might come back to this.
+    
     # for k, v in sorted(pdf.get_metadata().items()):
     #     if v:
     #         ret += "- %s = %s\n" % (k, parse_str(v).strip("/"))
