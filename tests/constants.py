bdd8a99a17170b7657e6707385e4459d4b5cf661
wiseaidev <business@wiseai.dev>
2022-10-03 13:20:25 +0300

    updated .gitignore, added mega.py, rm pdfs, cleanups
    Signed-off-by: wiseaidev <business@wiseai.dev>

---
+++ tests/constants.py
@@ -0,0 +1,9 @@
+PDFS = {
+    "email_test_single_page.pdf": "https://mega.nz/file/akRAXJhT#u5hWvL4JJmoz8JUQdJw4Nfnzw-WnXc2qwCAcVZwbLcM",
+    "embedded_link_image.pdf": "https://mega.nz/file/nsBgDSxT#dZIjpEb437SLxH-26P9V8cSNwVc9kTpqLFCP9x6CEeg",
+    "embedded_link_testcase.pdf": "https://mega.nz/file/C5gGTJJR#GUbraILCDX1j4QnVLEbtoKkzxBbJqN-AUBNWCXPFRS8",
+    "i14doc1.pdf": "https://mega.nz/file/u1owmDZa#xqeMALTWhA5UVyT7PRbevRzXmPU8pgBbjN0vzt6aRgo",
+    "i14doc2.pdf": "https://mega.nz/file/ehRwSBbC#BqaIZd4XKhdtmRguibffiTX8tA39m6cTSDx-OFFKQqs",
+    "invalid.pdf": "https://mega.nz/file/Hs4VHAII#dd9F6Uonk6c4tVnSO6VyeqQEkXh65EhMq6sT8zlIowY",
+    "valid.pdf": "https://mega.nz/file/Xp5RkbaI#3pRFF-kTQFh-ZwOs0O6JqqNdXhsjLuBNxZB2NDgl8Mc",
+}
