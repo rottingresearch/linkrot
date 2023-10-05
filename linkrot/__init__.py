34664e9b41ad7c212d5adc55c410a24895071dbe
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2023-10-05 19:37:34 -0400

    Update __init__.py

--- linkrot/__init__.py
+++ linkrot/__init__.py
@@ -27,13 +27,13 @@ linkrot can be used to extract info from PDF in two ways:
 https://github.com/rottingresearch/linkrot
 
 Copyright (c) 2023, Marshal Miller <marshal@rottingresearch.org>
-License: MIT (see LICENSE for details)
+License: GPLv3 (see LICENSE for details)
 """
 
 __title__ = "linkrot"
-__version__ = "4.0.3"
+__version__ = "4.0.4"
 __author__ = "Marshal Miller"
-__license__ = "GNU General Public License v3 (GPLv3)"
+__license__ = "GPLv3"
 __copyright__ = "Copyright 2023, Marshal Miller"
 
 import os
