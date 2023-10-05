206f01ae82c4769367dcde9609ffa7caa7da647a
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2023-10-05 19:32:04 -0400

    Update __init__.py

--- linkrot/__init__.py
+++ linkrot/__init__.py
@@ -31,9 +31,9 @@ License: MIT (see LICENSE for details)
 """
 
 __title__ = "linkrot"
-__version__ = "4.0.2"
+__version__ = "4.0.3"
 __author__ = "Marshal Miller"
-__license__ = "GPLv3"
+__license__ = "GNU General Public License v3 (GPLv3)"
 __copyright__ = "Copyright 2023, Marshal Miller"
 
 import os
