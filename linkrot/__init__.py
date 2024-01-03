681fc8f1c70d6af465aae9099acb4ca73c0d9964
Marshal Miller <marshal@marshalmiller.com>
2024-01-03 17:01:24 -0500

    Changes made to regex.

--- linkrot/__init__.py
+++ linkrot/__init__.py
@@ -31,7 +31,7 @@ License: GPLv3 (see LICENSE for details)
 """
 
 __title__ = "linkrot"
-__version__ = "5.0"
+__version__ = "5.1"
 __author__ = "Marshal Miller"
 __license__ = "GPL-3.0-or-later"
 __copyright__ = "Copyright 2024, Marshal Miller"
